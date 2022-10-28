4cb2e6bb4d4455eb4f2eca5ab6b0c062a2aea4c7
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-10-28 10:11:17 -0300

    #deploy

--- src/entry-point.ts
+++ src/entry-point.ts
@@ -1,4 +1,5 @@
 export {EnvBootstrap} from './application/env/env-bootstrap';
+export {EnvironmentFactory} from './application/env/factories/environment-factory';
 export {BufferEnvPresenter} from './application/env/presenters/buffer-env-presenter';
 export {NumberEnvPresenter} from './application/env/presenters/number-env-presenter';
 export {TextEnvPresenter} from './application/env/presenters/text-env-presenter';
